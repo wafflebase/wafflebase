0e05523d20df36e7bb59b6434b325687dd96ec99
Youngteac Hong <susukang98@gmail.com>
2024-04-14 23:54:18 +0900

    Add more TODOs

--- src/formula/functions.ts
+++ src/formula/functions.ts
@@ -4,15 +4,17 @@ import { FunctionContext } from '../../antlr/FormulaParser';
 /**
  * FunctionMap is a map of function name to the function implementation.
  */
-export const FunctionMap = new Map([['SUM', sumFunction]]);
+export const FunctionMap = new Map([['SUM', sum]]);
 
 /**
- * `sumFunction` is the implementation of the SUM function.
+ * `sum` is the implementation of the SUM function.
  */
-export function sumFunction(
+export function sum(
   ctx: FunctionContext,
   visit: (tree: ParseTree) => number,
 ): number {
+  // TODO(hackerwins): Sum should filter out non-numeric values.
+  // TODO(hackerwins): Sum must accpet at least 1 argument.
   let sum = 0;
   if (ctx.args()) {
     const args = ctx.args()!;
