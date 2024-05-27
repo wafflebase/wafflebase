9d503187cf482f4b8260b2c7c9e3170bb73d6781
Youngteac Hong <susukang98@gmail.com>
2024-05-27 23:58:20 +0900

    Add tungstenite for listening websocket

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
+use std::net::TcpListener;
+use tungstenite::accept;
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind("127.0.0.1:9001").unwrap();
+    println!("Server listening on port 9001");
+    for stream in server.incoming() {
+        let mut websocket = accept(stream.unwrap()).unwrap();
+        loop {
+            let msg = websocket.read_message().unwrap();
+            websocket.write_message(msg).unwrap();
+        }
+    }
 }
