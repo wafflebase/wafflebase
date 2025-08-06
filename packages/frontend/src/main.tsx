928d950310eb066132eb8185da350769b64ae9c8
Youngteac Hong <susukang98@gmail.com>
2025-08-06 17:45:38 +0900

    Bump up Yorkie to v0.6.24

--- packages/frontend/src/main.tsx
+++ packages/frontend/src/main.tsx
@@ -1,6 +1,10 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
+import { StrictMode } from "react";
 
-// TODO(hackerwins): Add StrictMode to the app.
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
