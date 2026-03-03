f700ed599d17b4bdf78fdae3e742f2a780a3a016
hackerwins <hackerwins@users.noreply.github.com>
2026-03-03 11:20:46 +0000

    deploy: ddeb58a5245b224d854f5ec45f41482163b037ae

---
+++ assets/createReactComponent-DHAdw0Za.js
@@ -0,0 +1,11 @@
+import{r as e}from"./vendor-react-Dv9abVRn.js";/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var p={outline:{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"none",stroke:"currentColor",strokeWidth:2,strokeLinecap:"round",strokeLinejoin:"round"},filled:{xmlns:"http://www.w3.org/2000/svg",width:24,height:24,viewBox:"0 0 24 24",fill:"currentColor",stroke:"none"}};/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const k=(r,s,a,w)=>{const o=e.forwardRef(({color:n="currentColor",size:i=24,stroke:c=2,title:l,className:m,children:t,...h},d)=>e.createElement("svg",{ref:d,...p[r],width:i,height:i,className:["tabler-icon",`tabler-icon-${s}`,m].join(" "),...r==="filled"?{fill:n}:{strokeWidth:c,stroke:n},...h},[l&&e.createElement("title",{key:"svg-title"},l),...w.map(([f,g])=>e.createElement(f,g)),...Array.isArray(t)?t:[t]]));return o.displayName=`${a}`,o};export{k as c};
