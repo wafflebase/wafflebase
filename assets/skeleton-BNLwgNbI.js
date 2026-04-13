1d5e09982519962064e5b0d91ddedb107da99a84
hackerwins <hackerwins@users.noreply.github.com>
2026-04-13 06:55:50 +0000

    deploy: 9946404ec62c1977fff4094cf7cdb2e55fca5f37

---
+++ assets/skeleton-BNLwgNbI.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-Dh8-tWa6.js";import{c as s}from"./index-DXAf-jUD.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
