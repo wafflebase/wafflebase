2b83c60fedaf3af1e52772ad0b05ff054d0f1bae
hackerwins <hackerwins@users.noreply.github.com>
2026-07-19 11:35:55 +0000

    deploy: 5daa1bfde9fd45bf2d2c04025ea6dfb2910a67e9

---
+++ assets/skeleton-BCuU-6QC.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-B-k5u3Ba.js";import{g as s}from"./index-rhf_ahAl.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
