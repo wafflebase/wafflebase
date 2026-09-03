c31db2129f32cdd98b74e56c478cc5e32c02345e
hackerwins <hackerwins@users.noreply.github.com>
2026-09-03 16:22:30 +0000

    deploy: 46918880a2ea1e652810f588fe4f8138304a7256

---
+++ assets/history-panel-lazy-CQ6tQRRw.js
@@ -0,0 +1,7 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/history-panel-aePwFbjU.js","assets/vendor-react-BcnGT3Lb.js","assets/input-DvwSc1nt.js","assets/vendor-ui-t4F5fC0M.js","assets/index-CHVhgh96.js","assets/index-Bj34gy0J.js","assets/slides-editor-engine-apLm8pbD.js","assets/sheet-core-XxT5Z_Lh.js","assets/sheet-formula-eval-C3W1Z9CX.js","assets/sheet-formula-parser-B9arzIpR.js","assets/vendor-app-B4AhiZG6.js","assets/vendor-yorkie-6-fo-DoE.js","assets/index-ZQlTC2O2.css","assets/vendor-ui-history-DDvUtVTs.js","assets/use-revision-history-BuGeFKAv.js"])))=>i.map(i=>d[i]);
+import{c as a}from"./createReactComponent-CVa0ZtGS.js";import{_ as e}from"./slides-editor-engine-apLm8pbD.js";import{j as o,r}from"./vendor-react-BcnGT3Lb.js";/**
+ * @license @tabler/icons-react v3.31.0 - MIT
+ *
+ * This source code is licensed under the MIT license.
+ * See the LICENSE file in the root directory of this source tree.
+ */var p=a("outline","history","IconHistory",[["path",{d:"M12 8l0 4l2 2",key:"svg-0"}],["path",{d:"M3.05 11a9 9 0 1 1 .5 4m-.5 5v-5h5",key:"svg-1"}]]);const s=r.lazy(()=>e(()=>import("./history-panel-aePwFbjU.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14])).then(t=>({default:t.HistoryPanel})));function m(t){return o.jsx(r.Suspense,{fallback:null,children:o.jsx(s,{...t})})}export{p as I,m as L};
