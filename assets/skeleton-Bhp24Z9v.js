5a09cde91d835a606704d8a6106d0268daba3209
hackerwins <hackerwins@users.noreply.github.com>
2026-09-05 00:23:15 +0000

    deploy: f092920699eacd39de6c174b07c9ca9fa2cfc07b

---
+++ assets/skeleton-Bhp24Z9v.js
@@ -0,0 +1,1 @@
+import{j as o}from"./vendor-react-BcnGT3Lb.js";import{v as s}from"./index-CZ6oeHax.js";function r({className:t,...e}){return o.jsx("div",{"data-slot":"skeleton",className:s("bg-accent animate-pulse rounded-md",t),...e})}export{r as S};
